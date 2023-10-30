795ae0e7e9dfdf617f5483ea4ba6200627f66578
Colin Kierans <vindexus@gmail.com>
2023-10-29 19:14:54 -0700

    Fix TS errors for kofi button

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -196,7 +196,7 @@ const Home = () => {
 						Open Trade Link
 					</a>
 					<a href='https://ko-fi.com/D1D8QMSKH' target='_blank'>
-						<img height='36' style={{border: '0px', 'height':'36px'}} src='https://storage.ko-fi.com/cdn/kofi1.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' />
+						<img height='36' style={{border: '0px', 'height':'36px'}} src='https://storage.ko-fi.com/cdn/kofi1.png?v=3' alt='Buy Me a Coffee at ko-fi.com' />
 					</a>
 				</div>
 			</div>
