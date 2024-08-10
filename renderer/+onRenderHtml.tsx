74f46c925ce22aaee377d44bc95b3fbc921c2830
Colin Kierans <vindexus@gmail.com>
2024-08-10 16:34:27 -0700

    Replace google analytics with withcabin take two

--- renderer/+onRenderHtml.tsx
+++ renderer/+onRenderHtml.tsx
@@ -34,14 +34,6 @@ const onRenderHtml: OnRenderHtmlAsync = async (pageContext): ReturnType<OnRender
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
         <title>${title}</title>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=${import.meta.env.VITE_GOOGLE_ANALYTICS_ID}"></script>
-				<script>
-					window.dataLayer = window.dataLayer || [];
-					function gtag(){dataLayer.push(arguments);}
-					gtag('js', new Date());
-			
-					gtag('config', '${import.meta.env.VITE_GOOGLE_ANALYTICS_ID}');
-				</script>
 				<script>
 				console.log('')
 				console.log('Contact:', 'contact@watcherseyesearch.com')
@@ -51,6 +43,7 @@ const onRenderHtml: OnRenderHtmlAsync = async (pageContext): ReturnType<OnRender
       </head>
       <body>
         <div id="react-root">${dangerouslySkipEscape(pageHtml)}</div>
+				<script async defer src="https://scripts.withcabin.com/hello.js"></script>
       </body>
     </html>`
 
